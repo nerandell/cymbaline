ac0c1922558ac333dea486be962babd87cc53114
nerandell <ankitchandawala@gmail.com>
2015-12-06 21:56:50 +0530

    Adding dummy tests

---
+++ tests/Cymbaline/Tests/CymbalineTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit_Framework_TestCase;
+
+class CymbalineTest extends PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(1==1);
+    }
+}
