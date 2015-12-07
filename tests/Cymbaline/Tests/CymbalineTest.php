72aed69494a77ed174edb797841d0d5334c5ec5f
nerandell <ankitchandawala@gmail.com>
2015-12-07 18:34:16 +0530

    Adding tests to add custom route

--- tests/Cymbaline/Tests/CymbalineTest.php
+++ tests/Cymbaline/Tests/CymbalineTest.php
@@ -2,10 +2,19 @@
 
 use PHPUnit_Framework_TestCase;
 
+use Cymbaline\Route;
+
 class CymbalineTest extends PHPUnit_Framework_TestCase
 {
-    public function testDummy()
+
+    function testAddCustomRoute()
     {
-        $this->assertTrue(1==1);
+        Route::addRoute('get', '/test_routes', function($val) {
+            return $val;
+        });
+        $this->assertEquals(sizeof(Route::getRoutes()), 1);
+        $this->assertEquals(Route::getRoutes()[0]['path'], '/test_routes');
+        $callback = Route::getRoutes()[0]['callback'];
+        $this->assertEquals($callback(42), 42);
     }
 }
